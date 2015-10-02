62336c4d26bf1c61b876b6c51d3f4f95de5bb6ad
tzelleke <theodros.zelleke@gmail.com>
2015-10-02 10:23:09 +0200

    Create setup.py. Add docs/requirements.txt to import sphinx theme on read-the-docs.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MinEnergyPath',
+    version='0.1',
+    packages=['MinEnergyPath'],
+    url='',
+    license='MIT',
+    author='Theodros Zelleke',
+    author_email='',
+    description=''
+)
